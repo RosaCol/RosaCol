e35602af2cf672ac10bcb8068b29cee13586cb22
RosaCol <rossaitzeel@gmail.com>
2020-06-12 21:53:32 -0500

    uuufff

--- Semana1/ejercicio3/ejercicio3.cpp
+++ Semana1/ejercicio3/ejercicio3.cpp
@@ -1,27 +1,46 @@
-#include<iostream>
-
+#include <iostream>
+#include <string>
 using namespace std;
 
-int people(int r)
-{
-int i;	
-int nd[r];	
-	for(i=0;i<r;i++)
-		cin>>nd[i];
-
-return nd[i];
+long* obt_z(long g){
+long* z=new long[g];
+for(int i=0;i<g;i++)
+cin>>z[i];
+return z;
 }
 
-int main()
-{
-	int a, b, c;
-cin>>a;
-cin>>b;
-cin>>c;
-int r=people(a);
-cout<<r<<endl;
-	int soli[a]={people(a)};
-	for(int i=0;i<a;i++)
-	cout<<soli[a]<<" ";
+long* obt_q(int x){
+long* q=new long[x];
+for(int i=0;i<x;i++)
+cin>>q[i];
+return q;
+}
+
+int obt_d(long* z,long* q,long g,int x,long k){
+int d=0;
+for(int i=0;i<x;i++){
+for(long j=0;j<g;j++){
+if((z[j]<=(q[i]+k))&&(z[j]>=(q[i]-k))){
+d+=1;q[i]=0;}
+}}
+
+return d;
+
+}
+
+int main(){
+
+int x;
+long g,k;
+cin>>g>>x>>k;
+long* z=new long[g];
+long* q=new long[x];
+if(g>=1&&x<=200000&&(0<=k<=1000000000)){
+z=obt_z(g);
+q=obt_q(x);
+int d=obt_d(z,q,g,x,k);
+cout<<d<<endl;
+}
 return 0;
+
 }
